a4faaec05ee07359f08f86da1af928a02e71641b
Pinak-Clg <24bit172@sot.pdpu.ac.in>
2025-04-06 20:08:38 +0530

    Update Lab 7 : sets.py

--- Lab 7 : sets.py
+++ Lab 7 : sets.py
@@ -1,4 +1,3 @@
-
 # 1. Write a program that converts words present in a list into uppercase and stores them in a set. 
 
 def list_to_set():
@@ -11,6 +10,10 @@ def list_to_set():
 
     print("Set with uppercase:",wordset)
 list_to_set()
+
+# output:
+# list:['apple', 'google', 'microsoft', 'atlassian', 'meta', 'google']
+# Set with uppercase:{'APPLE', 'GOOGLE', 'MICROSOFT', 'ATLASSIAN', 'META'}
 
 
 # 2. Write a program to create a set containing 10 random numbers in the range 15 to 45. Count how 
@@ -41,12 +44,18 @@ def set_modification():
 set_modification()
 
 
+# Sample Output:
+# set:{32, 17, 39, 22, 35, 40, 27, 18, 36, 19}
+# numbers less than 30:5
+# Set after removing numbers >35:{32, 35, 17, 18, 19, 22, 27}
+
+
 # 3. Create an empty set. Write a program that adds five new names to this set, modifies one existing 
 # name and deletes two names from it.
 
 def nameset():
     name = set()
-    name.update(["pinak", "rudra", "deep", "raj", "kiara"])
+    name.update(["pinak", "rudra","deep","raj","kiara"])
     print("set:",name)
 
     name.remove("kiara")
@@ -58,11 +67,14 @@ def nameset():
     print("set after deleting two names:",name)
 nameset()
 
+# output:
+# set:{'deep', 'raj', 'rudra', 'kiara','pinak'}
+# set after modifying a name:{'deep','raj', 'rudra', 'alia', 'pinak'}
+# set after deleting two names:{'deep','raj', 'alia'}
 
 
 # 4. A set contains names which begin either with A or with B. Write a program to separate out the 
 # names into two sets, one containing names beginning with A and another with B. 
-
 
 def separatenames():
     name = {"Aman","Ali", "Bili","Brijesh"}
@@ -81,4 +93,7 @@ def separatenames():
     print("Names B:",setB)
 separatenames()
 
-
+# output:
+# set:{'Aman', 'Ali', 'Brijesh', 'Bili'}
+# Names A:{'Aman', 'Ali'}
+# Names B:{'Brijesh', 'Bili'}
